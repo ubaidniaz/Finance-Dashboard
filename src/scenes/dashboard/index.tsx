0906e606a6430c46779519dac11eb7be2ba0f8e8
wisepakiguy <ubaidullahahsan@gmail.com>
2024-02-02 22:48:28 -0700

    dashboard functionality check/ color change

--- src/scenes/dashboard/index.tsx
+++ src/scenes/dashboard/index.tsx
@@ -1,7 +1,9 @@
+import { Box, useTheme } from '@mui/material';
 import React from 'react';
 
-const Dashboard: React.FC = () => {
-  return <div>Dashboard</div>;
+const Dashboard: React.FC = () => { 
+    const {palette} = useTheme();
+  return <Box color={palette.grey[300]}>Dashboard</Box>;
 };
 
 export default Dashboard;
