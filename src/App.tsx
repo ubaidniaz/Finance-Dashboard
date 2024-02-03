c9e58a82047421a8f4d888df09d87eb7e9af0e57
wisepakiguy <ubaidullahahsan@gmail.com>
2024-02-02 21:14:00 -0700

    setup react-router and create a box

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
 import { themeSettings } from "./theme"; 
-import { CssBaseline } from "@mui/material";
+import { Box, CssBaseline } from "@mui/material";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
 
   return (
     <div className="app"> 
+    <BrowserRouter>
+    
       <ThemeProvider theme={theme}>
         <CssBaseline />
+        <Box width ="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+
+        </Box>
       </ThemeProvider>
+    </BrowserRouter>
     </div>      
   );
 }
