359a595c8499e3616ef2a0850062660679fb2a1a
wisepakiguy <ubaidullahahsan@gmail.com>
2024-02-03 16:33:00 -0700

    import rechart components and copy oiver a simpleAreaChart

--- src/scenes/dashboard/Row1.tsx
+++ src/scenes/dashboard/Row1.tsx
@@ -1,6 +1,8 @@
 import DashboardBox from '@/components/DashboardBox';
 import { useGetKpisQuery } from '@/state/api';
 import React from 'react';
+import {ResponsiveContainer, AreaChart, CartesianGrid, XAxis, YAxis, Tooltip, Area} from "recharts";
+
 
 const Row1: React.FC = () => {
     const {data} = useGetKpisQuery();
@@ -8,7 +10,27 @@ const Row1: React.FC = () => {
 
   return ( 
     <>
-  <DashboardBox  gridArea="a"></DashboardBox>
+  <DashboardBox  gridArea="a">
+  <ResponsiveContainer width="100%" height="100%">
+        <AreaChart
+          width={500}
+          height={400}
+          data={data}
+          margin={{
+            top: 10,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
+        </AreaChart>
+      </ResponsiveContainer>
+  </DashboardBox>
   <DashboardBox  gridArea="b"></DashboardBox>
   <DashboardBox  gridArea="c"></DashboardBox>
   </>
