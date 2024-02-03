d4157b0cf7a160e782d8036bf46ee72987a95050
wisepakiguy <ubaidullahahsan@gmail.com>
2024-02-03 00:23:26 -0700

    grid box test

--- src/scenes/dashboard/index.tsx
+++ src/scenes/dashboard/index.tsx
@@ -11,20 +11,25 @@ const gridTemplate = `
  "g h i"
  "g h j"
  "g h j"
-`
+`;
 
 const Dashboard: React.FC = () => { 
     const {palette} = useTheme();
-  return(
-    <Box width="100%" height="100%" display="100%" gap="1.5rem" >
-        sx ={{
-            gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
-            gridTemplateRows: "repeat(10m minmax(60px, 1fr))",
-            gridTemplateAreas: gridTemplate,
-        }}
-    Dashboard
-    </Box>
-  );  
+    return(
+        <Box 
+            width="100%" 
+            height="100%" 
+            display="grid" 
+            gap="1.5rem"
+            sx={{
+                gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
+                gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
+                gridTemplateAreas: gridTemplate,
+            }}
+        >
+            <Box bgcolor="#fff" gridArea="a"></Box>
+        </Box>
+    );  
 };
 
 export default Dashboard;
