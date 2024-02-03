8877453597e8b67252dd74c2afc66a3645ae34d9
wisepakiguy <ubaidullahahsan@gmail.com>
2024-02-02 21:56:49 -0700

    navbar test with hey (success) (still errors)

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,16 +1,18 @@
 import {useState} from 'react';
 import { Link } from 'react-router-dom';
 import { Box, Typography, useTheme } from '@mui/material';
+import FlexBetween from '@/components/FlexBetween';
 
 
 const Navbar: React.FC = () => {
     const { palette } = useTheme();
   return (
-     <Box
-      display="flex" 
-      justifyContent="space-between" 
-      alignItems= "center" 
-     ></Box>
+     <FlexBetween
+      mb="0.25rem" 
+      p="0.5rem 0rem" 
+      color={palette.grey[300]}
+      >hey
+      </FlexBetween>
   );
 };
 
