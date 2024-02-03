a90e26db8f3b3d6a4f0e144ad6e08db213e04d07
wisepakiguy <ubaidullahahsan@gmail.com>
2024-02-02 22:10:51 -0700

    imported icon and made states for navbar

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,17 +1,23 @@
 import {useState} from 'react';
 import { Link } from 'react-router-dom';
+import EscalatorWarningIcon from '@mui/icons-material/EscalatorWarning';
 import { Box, Typography, useTheme } from '@mui/material';
 import FlexBetween from '@/components/FlexBetween';
 
 
 const Navbar: React.FC = () => {
     const { palette } = useTheme();
+    const [selected, setSelected] = useState("dashboard");
   return (
      <FlexBetween
       mb="0.25rem" 
       p="0.5rem 0rem" 
       color={palette.grey[300]}
-      >hey
+      >{/* LEFT SIDE */}
+      <FlexBetween gap="0.75rem">
+      <EscalatorWarningIcon />
+
+      </FlexBetween>
       </FlexBetween>
   );
 };
