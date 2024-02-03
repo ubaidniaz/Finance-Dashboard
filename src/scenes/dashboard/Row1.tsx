a069168c5ae7b59546919c431eadc78d869c9aa2
wisepakiguy <ubaidullahahsan@gmail.com>
2024-02-03 12:40:54 -0700

    update Row1.tsx to use Kpi query

--- src/scenes/dashboard/Row1.tsx
+++ src/scenes/dashboard/Row1.tsx
@@ -1,7 +1,10 @@
 import DashboardBox from '@/components/DashboardBox';
+import { useGetKpisQuery } from '@/state/api';
 import React from 'react';
 
 const Row1: React.FC = () => {
+    const {data} = useGetKpisQuery();
+
   return ( 
     <>
   <DashboardBox  gridArea="a"></DashboardBox>
