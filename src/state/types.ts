1519ddfdf70fc6ff06a967a8b8ff669386e297bf
wisepakiguy <ubaidullahahsan@gmail.com>
2024-02-03 17:04:04 -0700

    building interface types

---
+++ src/state/types.ts
@@ -0,0 +1,26 @@
+export interface ExpensesByCategory{
+    salaries: number;
+    supplies: number;
+    services: number;
+}
+
+export interface Month{
+    id: string;
+    month: string;
+
+    revenue: number;
+    expenses: number;
+    nonOperationalExpenses: number;
+    operationalExpenses: number;
+}
+
+export interface GetKpisResponse{
+    id: string;
+    _id: string;
+    __v: number;
+    totalProgit: number;
+    totalRevenue: number;
+    totalExpenses:number;
+    expensesByCategory: ExpensesByCategory;
+    monthlyData: Array<Month>;
+}
