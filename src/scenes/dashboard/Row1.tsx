ceaf55503b7a6fdc8e8f35a265cdc386f74b3529
wisepakiguy <ubaidullahahsan@gmail.com>
2024-02-03 21:58:02 -0700

    update margins

--- src/scenes/dashboard/Row1.tsx
+++ src/scenes/dashboard/Row1.tsx
@@ -31,14 +31,26 @@ const Row1: React.FC = () => {
           height={400}
           data={revenueExpenses}
           margin={{
-            top: 10,
-            right: 30,
-            left: 0,
-            bottom: 0,
+            top: 15,
+            right: 25,
+            left: -10,
+            bottom: 60,
           }}
         >
           <defs>
             <linearGradient id="colorRevenue" x1="0" y1="0" x2="0" y2="1" >
+              <stop
+               offset="5%"
+               stopColor={palette.primary[300]}
+               stopOpacity={0.5}
+              />
+              <stop
+               offset="95%"
+               stopColor={palette.primary[300]}
+               stopOpacity={0}
+              />
+            </linearGradient>
+            <linearGradient id="colorExpenses" x1="0" y1="0" x2="0" y2="1" >
               <stop
                offset="5%"
                stopColor={palette.primary[300]}
