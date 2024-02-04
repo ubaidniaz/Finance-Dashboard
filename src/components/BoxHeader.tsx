a5ddcd2dff7ae1dcaa6ad3bce563126356d04b97
wisepakiguy <ubaidullahahsan@gmail.com>
2024-02-04 02:46:48 -0700

    first header

--- src/components/BoxHeader.tsx
+++ src/components/BoxHeader.tsx
@@ -1,7 +1,41 @@
 import React from 'react';
+import FlexBetween from './FlexBetween';
+import { Box, Typography, useTheme } from '@mui/material';
 
-const BoxHeader: React.FC = () => {
-  return <div>BoxHeader</div>;
+type Props = {
+    title: string;
+    sideText: string;
+    subtitle?: string;
+    icon?: React.ReactNode;
+};
+
+const BoxHeader: React.FC<Props> = ({ icon, title, subtitle, sideText }) => {
+    const theme = useTheme();
+    return (
+        <FlexBetween 
+            color={theme.palette.grey[400]}
+            margin="1.5rem 1rem 0 1rem"
+        >
+            <FlexBetween>
+                {icon}
+                <Box width="100%">
+                    <Typography variant="h4" mb="-0.1rem">
+                        {title}
+                    </Typography>
+                    <Typography variant="h6">
+                        {subtitle}
+                    </Typography>
+                </Box>
+            </FlexBetween>
+            <Typography 
+                variant="h5" 
+                fontWeight="700" 
+                color={theme.palette.secondary[500]} 
+            >
+                {sideText}
+            </Typography>
+        </FlexBetween>
+    );
 };
 
 export default BoxHeader;
--- src/scenes/dashboard/Row1.tsx
+++ src/scenes/dashboard/Row1.tsx
@@ -1,3 +1,4 @@
+import BoxHeader from '@/components/BoxHeader';
 import DashboardBox from '@/components/DashboardBox';
 import { useGetKpisQuery } from '@/state/api';
 import { useTheme } from '@mui/material';
@@ -25,6 +26,11 @@ const Row1: React.FC = () => {
   return ( 
     <>
   <DashboardBox  gridArea="a">
+    <BoxHeader
+     title="Revenue and Expenses"
+     subtitle="top line represents revenue, bottom line represents expenses" 
+     sideText="+4%"
+      />
   <ResponsiveContainer width="100%" height="100%">
         <AreaChart
           width={500}
