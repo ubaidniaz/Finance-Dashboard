2be0112ec7bccb97fee13853c65871257c78df0b
wisepakiguy <ubaidullahahsan@gmail.com>
2024-02-04 22:19:03 -0700

    commit

--- src/state/api.ts
+++ src/state/api.ts
@@ -4,13 +4,17 @@ import { GetKpisResponse } from "./types";
 export const api = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
     reducerPath: "main",
-    tagTypes: ["Kpis"],
+    tagTypes: ["Kpis", "Products"],
     endpoints: (build) => ({
         getKpis: build.query<Array<GetKpisResponse>, void> ({
             query: () => "kpi/kpis/",
             providesTags: ["Kpis"] 
         }),
+        getProducts: build.query<Array<GetKpisResponse>, void> ({
+            query: () => "product/products/",
+            providesTags: ["Products"] 
+        }),
     }),
 });
 
-export const { useGetKpisQuery } = api; 
+export const { useGetKpisQuery, useGetProductsQuery } = api; 
