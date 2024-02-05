6a6542d5a61539e2df5303b979367915cde083c5
wisepakiguy <ubaidullahahsan@gmail.com>
2024-02-05 00:31:53 -0700

    update api/types

--- src/state/api.ts
+++ src/state/api.ts
@@ -1,18 +1,22 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { GetKpisResponse } from "./types";
+import { GetKpisResponse, GetProductsResponse, GetTransactionsResponse } from "./types";
 
 export const api = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
     reducerPath: "main",
-    tagTypes: ["Kpis", "Products"],
+    tagTypes: ["Kpis", "Products", "Transactions"],
     endpoints: (build) => ({
         getKpis: build.query<Array<GetKpisResponse>, void> ({
             query: () => "kpi/kpis/",
             providesTags: ["Kpis"] 
         }),
-        getProducts: build.query<Array<GetKpisResponse>, void> ({
+        getProducts: build.query<Array<GetProductsResponse>, void> ({
             query: () => "product/products/",
             providesTags: ["Products"] 
+        }),
+        getTransactions: build.query<Array<GetTransactionsResponse>, void> ({
+            query: () => "transaction/transactions/",
+            providesTags: ["Transactions"] 
         }),
     }),
 });
